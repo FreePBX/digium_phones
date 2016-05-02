c63b9c048340702d67fe2f1acb79a80c6dc1b65f
Scott Griepentrog <scott@griepentrog.com>
2016-05-02 17:04:29 -0500

    Fixed html5 issues including leaked tags

--- views/digium_phones_logos.php
+++ views/digium_phones_logos.php
@@ -81,24 +81,26 @@ if (isset($_GET['logo_upload']) && isset($_FILES['logo_upload']) && $_FILES['log
 
 ?>
 
-<table style="border-collapse:collapse; border-style:outset; border-width: 1px; margin-bottom: 20px;" cellpadding="5" cellspacing="0">
-<tr>
-<th style="border-style:inset; border-width:1px; "><a href="#" class="info">Logo Name<span>A Logo's named identifier.</span></a></th>
-<th style="border-style:inset; border-width:1px; "><a href="#" class="info">Phone Model<span>The Digium phone model to which this logo may apply</span></a></th>
-<th style="border-style:inset; border-width:1px; width: 210px; "><a href="#" class="info">Image<span>A preview of the logo image</span></a></th>
-<th style="border-style:inset; border-width:1px; "><a href="#" class="info">Actions<span>"Edit" provides additional editing control over a selected logo. "Delete" removes the specified logo.</span></a></th>
-</tr>
+<table style="border-collapse:collapse; border-style:outset; border-width: 1px; margin-bottom: 20px;">
+	<tr>
+		<th style="border-style:inset; border-width:1px; "><a href="#" class="info">Logo Name<span>A Logo's named identifier.</span></a></th>
+		<th style="border-style:inset; border-width:1px; "><a href="#" class="info">Phone Model<span>The Digium phone model to which this logo may apply</span></a></th>
+		<th style="border-style:inset; border-width:1px; width: 210px; "><a href="#" class="info">Image<span>A preview of the logo image</span></a></th>
+		<th style="border-style:inset; border-width:1px; "><a href="#" class="info">Actions<span>"Edit" provides additional editing control over a selected logo. "Delete" removes the specified logo.</span></a></th>
+	</tr>
 <?php
 if (empty($logos)) {
 ?>
-	<tr><td colspan="4">No custom logos configured.</td></tr>
+	<tr>
+		<td colspan="4">No custom logos configured.</td>
+	</tr>
 <?php
 }
 foreach ($logos as $logo) {
 ?>
-<tr>
-	<td><?php echo $logo['name']?></td>
-	<td><?php echo strtoupper($logo['model'])?></td>
+	<tr>
+		<td><?php echo $logo['name']?></td>
+		<td><?php echo strtoupper($logo['model'])?></td>
 <?php
 	if (file_exists("{$amp_conf['ASTETCDIR']}/digium_phones/user_image_{$logo['id']}.png")) {
 ?>
@@ -110,11 +112,14 @@ foreach ($logos as $logo) {
 <?php
 	}
 ?>
-	<td>
-		<button type="button" onclick="edit_logo_clicked(<?php echo $logo['id']?>);">Edit</button>
-		<form name="digium_phones_logos" method="post" action="config.php?type=setup&display=digium_phones&digium_phones_form=logos_edit&deletepng=<?php echo $logo['id']?>"><input type="submit" value="Delete"></form>
-	</td>
-</tr>
+		<td>
+			<button type="button" onclick="edit_logo_clicked(<?php echo $logo['id']?>);">Edit</button>
+
+			<form name="digium_phones_logos" method="post" action="config.php?type=setup&display=digium_phones&digium_phones_form=logos_edit&deletepng=<?php echo $logo['id']?>">
+				<input type="submit" value="Delete">
+			</form>
+		</td>
+	</tr>
 <?php
 }
 ?>
@@ -133,68 +138,63 @@ if ($rc) {
 <input type="submit" name="add_logo_submit" value="Add Logo" onclick="add_logo_clicked();"/>
 
 <div id="divaddlogo" style="display: none;">
-<hr style="margin-top: 30px;"/>
-<h2>Add New Logo</h2>
-<form name="digium_phones_logos" method="post" enctype="multipart/form-data" action="config.php?type=setup&display=digium_phones&digium_phones_form=logos_edit&logo_upload=yes">
-<table cellpadding="5">
-<tr>
-	<td><a href="#" class="info">Logo Name<span>A named identifier for the Logo.</span></a></td>
-	<td><input type="text" id="logo_name" name="logo_name" /></td>
-</tr>
-<tr>
-	<td><a href="#" class="info">Phone Model<span>Select the Digium phone model which can use this logo.</span></a></td>
-	<td>
-		<select id="logo_model" name="logo_model" />
-		<?php phone_model_options() ?>
-		</select>
-	</td>
-</tr>
-<tr>
-	<td colspan="2">
-	<input type="file" name="logo_upload" />
-	</td>
-</tr>
-<tr>
-	<td colspan="2">
-
-	<input type="submit" name="logo_upload_submit" value="Upload"/>
+	<hr style="margin-top: 30px;"/>
+	<h2>Add New Logo</h2>
+	<form name="digium_phones_logos" method="post" enctype="multipart/form-data" action="config.php?type=setup&display=digium_phones&digium_phones_form=logos_edit&logo_upload=yes">
+		<table style="border-spacing: 4px;">
+			<tr>
+				<td><a href="#" class="info">Logo Name<span>A named identifier for the Logo.</span></a></td>
+				<td><input type="text" id="logo_name" name="logo_name" /></td>
+			</tr>
+			<tr>
+				<td><a href="#" class="info">Phone Model<span>Select the Digium phone model which can use this logo.</span></a></td>
+				<td>
+					<select id="logo_model" name="logo_model"><?php phone_model_options() ?></select>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="file" name="logo_upload" /></td>
+			</tr>
+			<tr>
+				<td colspan="2"> 
+					<input type="submit" name="logo_upload_submit" value="Upload"/>
+					<button type="button" onclick="add_logo_clicked();">Cancel</button>
+				</td>
+			</tr>
+		</table>
 	</form>
-	<button type="button" onclick="add_logo_clicked();">Cancel</button>
-	</td>
-</tr>
-</table>
 </div>
 
 
 <div id="diveditlogo" style="display: none;">
-<hr style="margin-top: 30px;"/>
-<h2>Edit Logo</h2>
-<form name="digium_phones_edit_logos" method="post" enctype="multipart/form-data" action="config.php?type=setup&display=digium_phones&digium_phones_form=logos_edit&logo_upload=yes">
-<table cellpadding="5">
-<tr>
-	<td><a href="#" class="info">Logo Name<span>Named identifier for the Logo.</span></a></td>
-	<td><input type="text" id="edit_logo_name" name="edit_logo_name" /></td>
-</tr>
-<tr>
-	<td><a href="#" class="info">Phone Model<span>Select the Digium phone model which can use this logo.  Logo files should be PNG format, 8-bit, no transparency, and less than 10k in file size.  Dimensions for D40 and D50 logos: 150x45 pixels.  Dimensions for D70 logos: 205x85 pixels.</span></a></td>
-	<td>
-		<select id="edit_logo_model" name="edit_logo_model" />
-		<?php phone_model_options() ?>
-		</select>
-	</td>
-</tr>
-<tr>
-	<td colspan="2">
-	<input type="hidden" id="edit_logo_id" name="edit_logo_id"/>
-	<input type="file" id="logo_upload" name="logo_upload" value="Upload"/>
-	</td>
-</tr>
-<tr>
-	<td colspan="2">
-	<input type="submit" name="edit_logo_upload_submit" value="Save"/>
+	<hr style="margin-top: 30px;"/>
+	<h2>Edit Logo</h2>
+	<form name="digium_phones_edit_logos" method="post" enctype="multipart/form-data" action="config.php?type=setup&display=digium_phones&digium_phones_form=logos_edit&logo_upload=yes">
+		<table style="border-spacing: 4px">
+			<tr>
+				<td><a href="#" class="info">Logo Name<span>Named identifier for the Logo.</span></a></td>
+				<td><input type="text" id="edit_logo_name" name="edit_logo_name" /></td>
+			</tr>
+			<tr>
+				<td><a href="#" class="info">Phone Model<span>Select the Digium phone model which can use this logo.  Logo files should be PNG format, 8-bit, no transparency, and less than 10k in file size.  Dimensions for D40 and D50 logos: 150x45 pixels.  Dimensions for D70 logos: 205x85 pixels.</span></a></td>
+				<td>
+					<select id="edit_logo_model" name="edit_logo_model">
+					<?php phone_model_options() ?>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="hidden" id="edit_logo_id" name="edit_logo_id"/>
+					<input type="file" id="logo_upload" name="logo_upload" />
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="submit" name="edit_logo_upload_submit" value="Save"/>
+					<button type="button" onclick="edit_logo_clicked();">Cancel</button>
+				</td>
+			</tr>
+		</table>
 	</form>
-	<button type="button" onclick="edit_logo_clicked();">Cancel</button>
-	</td>
-</tr>
-</table>
 </div>

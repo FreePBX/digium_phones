40221818750419ddce827982668e31d7dad1121b
Scott Griepentrog <scott@griepentrog.com>
2016-05-04 10:22:52 -0500

    Various html5 violations fixed

--- views/digium_phones_alerts.php
+++ views/digium_phones_alerts.php
@@ -1,8 +1,3 @@
-<style type="text/css">
-tr.rowcolor0 { bgcolor:red; }
-tr.rowcolor1 { bgcolor:blue; }
-</style>
-
 <h2>Alerts</h2>
 <hr />
 
@@ -71,14 +66,14 @@ function handleAlertTypeChange(select_name, hide_control_name) {
 	}
 }
 </script>
-<table style="border-collapse:collapse; border-style:outset; border-width: 1px; margin-bottom: 20px;" cellpadding="5" cellspacing="0">
+<form name="digium_phones_alerts_del" id="digium_phones_alerts_del" method="post" enctype="multipart/form-data" action="config.php?type=setup&display=digium_phones&digium_phones_form=alerts_edit">
+<table style="border-collapse:collapse; border-style:outset; border-width: 1px; margin-bottom: 20px; border-spacing: 0px">
 <tr>
 <th style="border-style:inset; border-width:1px; "><a href="#" class="info">Alert Name<span>Named identifiers of configured Alerts.</span></a></th>
 <th style="border-style:inset; border-width:1px; "><a href="#" class="info">Ringing Type<span>Ringing Type of the Alert</span></a></th>
 <th style="border-style:inset; border-width:1px; "><a href="#" class="info">Ringtone<span>The ringing tone for the Alert</span></a></th>
 <th style="border-style:inset; border-width:1px; "><a href="#" class="info">Actions<span>"Edit" provides additional editing control over a selected Alert. "Delete" removes the specified Alert.</span></a></th>
 </tr>
-<form name="digium_phones_alerts_del" id="digium_phones_alerts_del" method="post" enctype="multipart/form-data" action="config.php?type=setup&display=digium_phones&digium_phones_form=alerts_edit">
 <?php
 // we need or global $alerts above
 if (empty($alerts)) {
@@ -87,13 +82,13 @@ if (empty($alerts)) {
 $i = 0;
 foreach ($alerts as $alert) {
 	?>
-	<tr style="border:1px solid #D9E6EE;" bgcolor="<?php echo ($i % 2 == 0) ? "#EEEEEE" : "#FFFFFF"; $i++; ?>">
+	<tr style="border:1px solid #D9E6EE;">
 	<td><?php echo $alert['name']?></td>
 	<td><?php echo $alert['type']?></td>
 	<td><?php if ($alert['type'] !== 'answer' and $alert['type'] !== 'visual') {
 		echo $alert['ringtone_name'];
 	}?></td>
-	<td valign="top" style="border-style:inset; border-width:0px; white-space: nowrap;" />
+	<td style="vertical-align: middle; border-style:inset; border-width:0px; white-space: nowrap;">
 		<input type="button" value="Edit" onclick="edit_alert_clicked(<?php echo $alert['id']?>);" />
 		<input type="submit" name="alertDelSubmit" id="alertDelSubmit" value="Delete" onclick="createHiddenInput('digium_phones_alerts_del', 'hiddenIdDel', '<?php echo $alert['id']?>');" />
 	</td>
@@ -102,14 +97,14 @@ foreach ($alerts as $alert) {
 }
 ?>
 </table>
-<input type="button" value="Add Alert" name="add_alert_submit" value="Add Alert" onclick="add_alert_clicked();"/>
+<input type="button" value="Add Alert" name="add_alert_submit" onclick="add_alert_clicked();"/>
 </form>
 
 <div id="divaddalert" style="display: none;">
 <hr style="margin-top: 30px;"/>
 <h2>Add New Alert</h2>
 <form name="digium_phones_alerts_add" method="post" enctype="multipart/form-data" action="config.php?type=setup&display=digium_phones&digium_phones_form=alerts_edit">
-<table cellpadding="5">
+<table>
 <tr>
 	<td><a href="#" class="info">Alert Name<span>The named identifier for this Alert.</span></a></td>
 	<td><input type="text" id="alertAddName" name="alertAddName" onkeyup="checkIsFilled('alertAddName', 'alertAddSubmit')" /></td>
@@ -121,7 +116,7 @@ foreach ($alerts as $alert) {
 <tr>
 	<td><a href="#" class="info">Ringing Type<span>The type of ringing behavior assigned for the alert.  Normal is a normal ring, where the phone plays a tone and waits to be answered.  Answer causes the phone to immediately go off hook into speaker or headset mode and answer the call.  Ring-Answer causes the phone to play a short version of the ring tone and to then go off-hook into speaker or headset mode and answer the call.  Visual causes the phone to silently ring, no tone is played.</span></a></td>
 	<td>
-		<select id="alertAddType" name="alertAddType" onchange="handleAlertTypeChange('#alertAddType', '#alertAddRingtone');"/>
+		<select id="alertAddType" name="alertAddType" onchange="handleAlertTypeChange('#alertAddType', '#alertAddRingtone');">
 			<option value="normal">normal</option>
 			<option value="answer">answer</option>
 			<option value="ring-answer">ring-answer</option>
@@ -132,7 +127,7 @@ foreach ($alerts as $alert) {
 <tr>
 	<td><a href="#" class="info">Ringtone<span>The ringtone, one of the phone's built-in defaults or one of the user-specified custom ringtones, to be played for the Alert.</span></a></td>
 	<td>
-                <select id="alertAddRingtone" name="alertAddRingtone" />
+                <select id="alertAddRingtone" name="alertAddRingtone">
 		<?php
                 foreach ($ringtones as $row) {
 			?>
@@ -145,7 +140,7 @@ foreach ($alerts as $alert) {
 </tr>
 <tr>
 	<td colspan="2">
-	<input type="submit" name="alertAddSubmit" id="alertAddSubmit" value="Save" disabled=true />
+	<input type="submit" name="alertAddSubmit" id="alertAddSubmit" value="Save" disabled />
 	<button type="button" onclick="add_alert_clicked();">Cancel</button>
 	</td>
 </tr>
@@ -157,7 +152,7 @@ foreach ($alerts as $alert) {
 <hr style="margin-top: 30px;"/>
 <h2>Edit Alert</h2>
 <form name="digium_phones_alerts_edit" method="post" enctype="multipart/form-data" action="config.php?type=setup&display=digium_phones&digium_phones_form=alerts_edit">
-<table cellpadding="5">
+<table>
 <tr>
 	<td><a href="#" class="info">Alert Name<span>Name given to this alert file for easier reference.</span></a></td>
 	<td><input type="text" id="alertEditName" name="alertEditName" onkeyup="checkIsFilled('alertEditName', 'alertEditSubmit')" /></td>
@@ -169,7 +164,7 @@ foreach ($alerts as $alert) {
 <tr>
         <td><a href="#" class="info">Ringing Type<span>Something descriptive should go here.</span></a></td>
         <td>
-                <select id="alertEditType" name="alertEditType" onchange="handleAlertTypeChange('#alertEditType', '#alertEditRingtoneId');" />
+                <select id="alertEditType" name="alertEditType" onchange="handleAlertTypeChange('#alertEditType', '#alertEditRingtoneId');">
                         <option value="normal">normal</option>
                         <option value="answer">answer</option>
                         <option value="ring-answer">ring-answer</option>
@@ -180,7 +175,7 @@ foreach ($alerts as $alert) {
 <tr>
         <td><a href="#" class="info">Ringtone<span>Something descriptive should go here.</span></a></td>
         <td>
-                <select id="alertEditRingtoneId" name="alertEditRingtoneId" />
+                <select id="alertEditRingtoneId" name="alertEditRingtoneId">
 		<?php
                 foreach ($ringtones as $row) {
 			?>
